bdd490740867ee2da4e4b1f0e57873afb119bd09
John Otander <johnotander@gmail.com>
2015-10-30 15:37:24 -0600

    Update packages, use ava and standard style
    Use standard style

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 
-var postcss = require('postcss');
+var postcss = require('postcss')
 
-var calc = require('./lib/calc');
-var media = require('./lib/custom-media');
-var variables = require('./lib/variables');
+var calc = require('./lib/calc')
+var media = require('./lib/custom-media')
+var variables = require('./lib/variables')
 
-module.exports = function(src, options) {
+module.exports = function (src, options) {
 
-  var options = options || {};
-  var root = postcss.parse(src);
-  var mediaVarsString = '';
-  var varsString = '';
+  var options = options || {}
+  var root = postcss.parse(src)
+  var mediaVarsString = ''
+  var varsString = ''
 
-  root = calc(root);
+  root = calc(root)
 
-  var obj = media(root);
-  root = obj.root;
-  mediaVarsString = obj.definitions;
+  var obj = media(root)
+  root = obj.root
+  mediaVarsString = obj.definitions
 
-  var obj = variables(root);
-  root = obj.root;
-  varsString = obj.definitions;
+  var obj = variables(root)
+  root = obj.root
+  varsString = obj.definitions
 
-  var scss = postcss().process(root).css;
+  var scss = postcss().process(root).css
 
-  scss = varsString + '\n' + mediaVarsString + '\n' + scss;
+  scss = varsString + '\n' + mediaVarsString + '\n' + scss
 
-  return scss;
+  return scss
 
-};
+}
 
